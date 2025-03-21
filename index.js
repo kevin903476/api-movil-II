b90bb04ed5519fa432259e6fa7f8f1fa9b5489d8
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 21:54:20 -0600

    Cambio de puerto
    Co-authored-by: kevin903476 <kevin903476@users.noreply.github.com>

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use('/items', itemRoutes);
 app.use('/users', userRoutes);
 
