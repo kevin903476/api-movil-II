8c2c5c7a2f631f598703748c071924d7e6a30751
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 21:18:44 -0600

    Comentar rutas de items y usuarios en index.js y agregar ruta de bienvenida

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 
-const itemRoutes = require('./routes/itemRoutes');
-const userRoutes = require('./routes/userRoutes'); 
+/* const itemRoutes = require('./routes/itemRoutes');
+const userRoutes = require('./routes/userRoutes');  */
 
 dotenv.config();
 const app = express();
@@ -13,8 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+/* 
 app.use('/items', itemRoutes);
 app.use('/users', userRoutes); 
+ */
+app.get('/', (req, res) => {
+    res.send('Welcome to the REST API!');
+  });
 
 module.exports = app;
