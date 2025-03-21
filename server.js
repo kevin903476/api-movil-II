d00b81b37010fc382918f2872933b0fd7e0deb35
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 21:32:05 -0600

    Escuchar en todas las interfaces al iniciar el servidor en server.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
