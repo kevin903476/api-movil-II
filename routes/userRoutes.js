09cc764e5079b4bb654fcbf1226822dc8e0552dd
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 20:30:37 -0600

    Modificar ruta para obtener todos los usuarios: cambiar de '/' a '/getAllUsers'

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const userController = require('../controllers/userRegisterController');
 
 router.post('/register', userController.registerUser);
-router.get('/', userController.getAllUsers);
+router.get('/getAllUsers', userController.getAllUsers);
 module.exports = router;
