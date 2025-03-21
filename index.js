4511a07a624386e21e4e97f11bc0f4016b6533f7
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 20:37:09 -0600

    cambio de nombre a archivo de app.js a index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+const itemRoutes = require('./routes/itemRoutes');
+const userRoutes = require('./routes/userRoutes'); 
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/items', itemRoutes);
+app.use('/users', userRoutes); 
+
+module.exports = app;
