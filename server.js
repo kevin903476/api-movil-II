de3bd2c6eedb5c2db2b27eb367376cfb425df242
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-20 20:51:47 -0600

    Eliminar valor por defecto del puerto en server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('.');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
