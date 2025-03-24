6bbfef0bb68a4fe0e42ff5795a5d113166e5cee2
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-24 15:34:47 -0600

    fix: corregir la importación del validador en las rutas de usuario

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userRegisterController');
 const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
-const  validateRegisterEstudiante = require('../middleware/validator');
+const  { validateRegisterEstudiante } = require('../middleware/validator');
 
 router.get('/getAllUsers', userController.getAllUsers);
 router.post('/registerEstudiante',validateRegisterEstudiante, userController.registerEstudiante);
