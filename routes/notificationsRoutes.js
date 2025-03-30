3e49420ba67365d0336c1149cb5cb97d1810a8e7
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-29 22:50:46 -0600

    fix: corregir nombre del controlador de notificaciones

--- routes/notificationsRoutes.js
+++ routes/notificationsRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notificationController = require('../controllers/notificationController');
+const notificationController = require('../controllers/notificationsController');
 const auth = require('../middleware/auth');
 
 router.post('/register-token', auth, notificationController.registerPushToken);
