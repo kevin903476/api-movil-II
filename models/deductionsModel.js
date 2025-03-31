d54edd0cc513d8a89b4270fa9b7aebc8c23f271e
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-31 15:15:34 -0600

    fix: modificar la función getDetailsBillProfessor para devolver un array vacío en lugar de null

--- models/deductionsModel.js
+++ models/deductionsModel.js
@@ -17,10 +17,7 @@ class DeductionsModel { //deducciones se relaciona con facturas y pagos
         try {
             const result = await db.query('SELECT * FROM vista_detalle_facturas WHERE profesor_id = ?', [profesor_id]);
             console.log('Resultado de getDetailsBillProfesssor:', result);
-            if (result[0] && result[0].length > 0) {
-                return result[0][0]; // Devuelve el primer registro
-            }
-            return null;
+            return result && result.length > 0 ? result : [];
         } catch (error) {
             console.error('Error en getDetailsBillProfesssor:', error);
             throw error;
