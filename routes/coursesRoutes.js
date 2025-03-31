e3a35d4d7f7f4521fbda7d7f399e7e4246422a9d
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-03-31 14:00:51 -0600

    fix: agregar autenticación y verificación de roles en las rutas de cursos Co-authored-by: kevin903476 <kevin903476@users.noreply.github.com>

--- routes/coursesRoutes.js
+++ routes/coursesRoutes.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const coursesController = require('../controllers/coursesController');
+const auth = require('../middleware/auth');
+const checkRole = require('../middleware/checkRole');
 
-router.get('/getCourses', coursesController.getCourses );
-router.post('/createCourse', coursesController.createCourse );
+router.get('/getCourses', auth,checkRole([1,2,3]), coursesController.getCourses );
+router.post('/createCourse', auth, checkRole([1, 3]), coursesController.createCourse );
 
 // logica para agregar un profe a un curso
 
-router.post('/insertCourseScheduleProfessor', coursesController.insertCourseScheduleProfessor );
+router.post('/insertCourseScheduleProfessor', auth, checkRole([1, 3]), coursesController.insertCourseScheduleProfessor );
 
 
 module.exports = router;
