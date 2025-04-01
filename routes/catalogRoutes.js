e3a58954fda423480df1b8a137dd8cf9caf1f23f
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-01 17:42:43 -0600

    feat: actualizar rutas del catálogo para requerir autenticación

--- routes/catalogRoutes.js
+++ routes/catalogRoutes.js
@@ -5,15 +5,14 @@ const catalogController = require('../controllers/catologosController');
 const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
 
-//rutas publicas para el frontend sin autenticacion
-router.get('/universities', catalogController.getCatalogUniversity);
-router.get('/courses', auth,checkRole([1,2,3]) ,catalogController.getCatalogCourse);
-router.get('/headquarters', catalogController.getCatalogHeadquarters);
-router.get('/careers',  auth,checkRole([1,2,3]), catalogController.getCatalogCareer);
-router.get('/roles', auth,checkRole([1,2,3]), catalogController.getCatalogRoles);
-router.get('/roles', auth,checkRole([1,2,3]), catalogController.getCatalogRoles);
-router.get('/enclosures', auth,checkRole([1,2,3]),catalogController.getCatalogEnclosure);
-router.get('/coupons', auth,checkRole([1,2,3]),catalogController.getCatalogCoupons);
+//rutas publicas para el frontend con autenticacion
+router.get('/universities', auth, checkRole([1, 2, 3]), catalogController.getCatalogUniversity);
+router.get('/courses', auth, checkRole([1, 2, 3]), catalogController.getCatalogCourse);
+router.get('/careers', auth, checkRole([1, 2, 3]), catalogController.getCatalogCareer);
+router.get('/roles', auth, checkRole([1, 2, 3]), catalogController.getCatalogRoles);
+router.get('/roles', auth, checkRole([1, 2, 3]), catalogController.getCatalogRoles);
+router.get('/enclosures', auth, checkRole([1, 2, 3]), catalogController.getCatalogEnclosure);
+router.get('/coupons', auth, checkRole([1, 2, 3]), catalogController.getCatalogCoupons);
 router.get('/universitiesWithAllInfo', auth, checkRole([1, 2, 3]), catalogController.getUniversitiesWithAllInfo);
 
 module.exports = router;
