3628f531534a8b5fd1035ad6e3161e86f36f56e3
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-01 17:39:47 -0600

    feat: agregar autenticación y control de roles en las rutas del catálogo

--- routes/catalogRoutes.js
+++ routes/catalogRoutes.js
@@ -7,12 +7,13 @@ const checkRole = require('../middleware/checkRole');
 
 //rutas publicas para el frontend sin autenticacion
 router.get('/universities', catalogController.getCatalogUniversity);
-router.get('/courses', catalogController.getCatalogCourse);
+router.get('/courses', auth,checkRole([1,2,3]) ,catalogController.getCatalogCourse);
 router.get('/headquarters', catalogController.getCatalogHeadquarters);
-router.get('/careers', catalogController.getCatalogCareer);
-router.get('/roles', catalogController.getCatalogRoles);
-router.get('/enclosures', catalogController.getCatalogEnclosure);
-router.get('/coupons', catalogController.getCatalogCoupons);
+router.get('/careers',  auth,checkRole([1,2,3]), catalogController.getCatalogCareer);
+router.get('/roles', auth,checkRole([1,2,3]), catalogController.getCatalogRoles);
+router.get('/roles', auth,checkRole([1,2,3]), catalogController.getCatalogRoles);
+router.get('/enclosures', auth,checkRole([1,2,3]),catalogController.getCatalogEnclosure);
+router.get('/coupons', auth,checkRole([1,2,3]),catalogController.getCatalogCoupons);
 router.get('/universitiesWithAllInfo', auth, checkRole([1, 2, 3]), catalogController.getUniversitiesWithAllInfo);
 
 module.exports = router;
