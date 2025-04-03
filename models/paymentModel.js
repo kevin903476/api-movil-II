5b2c73c076d696ccb1d582ecad66a1ce54f79caa
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-02 23:40:08 -0600

    fix: corregir errores en consultas de pagos pendientes y pagos recibidos de profesores

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -6,7 +6,7 @@ class PaymentModel {
 
     async pendingPaymentsProfessor(profesor_id) {
         try {
-            const result = await db.query('SELECT * FROM vista_pagos_pendientes_recibidos_profesor WHERE profesor_id = ?'[profesor_id]);
+            const result = await db.query('SELECT * FROM vista_pagos_pendientes_recibidos_profesor WHERE profesor_id = ?',[profesor_id]);
             console.log('Resultado de pendingPaymentsProfessor:', result);
             if (result[0] && result[0].length > 0) {
               return result[0][0]; // Devuelve el primer registro
@@ -21,7 +21,7 @@ class PaymentModel {
     async getPaymentsProfessor() {
         try {
             const result = await db.query('SELECT * FROM vista_pagos_recibidos_profesor');
-            const pagos result;
+            const pagos = result;
             console.log('Resultado de la consulta:', pagos);
             return pagos;
         } catch (error) {
@@ -32,7 +32,7 @@ class PaymentModel {
 
     async pendingPaymentsStudent(estudiante_id) {
         try {
-            const result = await db.query('SELECT * FROM vista_pagos_pendientes_estudiante WHERE estudiante_id = ?'[estudiante_id]);
+            const result = await db.query('SELECT * FROM vista_pagos_pendientes_estudiante WHERE estudiante_id = ?',[estudiante_id]);
             console.log('Resultado de pendingPaymentsStudent:', result);
             if (result[0] && result[0].length > 0) {
               return result[0][0]; // Devuelve el primer registro
