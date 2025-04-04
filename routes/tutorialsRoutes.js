2190ec2ecafadf9298ff0bcd73ea027322591fc9
kevin903476 <73252529+kevin903476@users.noreply.github.com>
2025-04-03 20:01:22 -0600

    fix: change getTutorialsProfessorCourse endpoint from GET to POST

--- routes/tutorialsRoutes.js
+++ routes/tutorialsRoutes.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
 
 router.get('/getTutorials', auth,checkRole([1,2,3]), tutorialsController.getTutorials );
-router.get('/getTutorialsProfessorCourse', auth,checkRole([1,3]), tutorialsController.getTutorialsProfessorCourse );
+router.post('/getTutorialsProfessorCourse', auth,checkRole([1,3]), tutorialsController.getTutorialsProfessorCourse );
 router.get('/getScheduledTutorials', auth,checkRole([1,2,3]), tutorialsController.getScheduledTutorials );
 router.post('/scheduleTutoring', auth, checkRole([1, 2]), tutorialsController.scheduleTutoring );
 
