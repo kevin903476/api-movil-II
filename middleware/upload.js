040c4c9ec38df529d5f6e277cd02f931918547e7
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-07 00:54:16 -0600

    fix: corregir la ruta de destino para las subidas de archivos

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'public/uploads/'),
+  destination: (req, file, cb) => cb(null, '../public/uploads'),
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}${path.extname(file.originalname)}`;
     cb(null, uniqueName);
