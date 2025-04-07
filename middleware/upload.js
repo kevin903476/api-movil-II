5d34876e2f72294a880f1f807486cba13373c9e9
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-07 02:24:43 -0600

    fix: simplificar la lógica de creación del directorio de subida y mejorar la generación de nombres de archivo

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,22 +1,14 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-
-const uploadPath = path.join(__dirname, '../public/uploads');
-
-// Verifica si el directorio existe, de lo contrario lo crea
-if (!fs.existsSync(uploadPath)) {
-    fs.mkdirSync(uploadPath, { recursive: true });
-}
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadPath);
-  },
+  destination: (req, file, cb) => cb(null, path.join(__dirname, 'public/uploads')),
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    const uniqueName = `${Date.now()}-${file.originalname.replace(/\s+/g, '-')}`;
+    cb(null, uniqueName);
   }
 });
 
 const upload = multer({ storage });
+
 module.exports = upload;
