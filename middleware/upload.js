c3cdcee910a7ee10d059258e33bdeb4646e00861
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-07 01:20:53 -0600

    fix: simplificar la verificación y creación del directorio de subida

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,17 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, '../public/uploads');
+
+// Verifica si el directorio existe, de lo contrario lo crea
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // Verifica que la ruta sea correcta y que la carpeta exista
-    cb(null, path.join(__dirname, '../public/uploads'));
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + '-' + file.originalname);
