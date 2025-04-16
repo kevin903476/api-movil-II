deebdfbf0e6bbc5403cdd5ec5cf51a9821281de2
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-15 21:05:57 -0600

    fix: upload.js many errors

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,14 +4,12 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 
-// Configuración de Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Storage de Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
