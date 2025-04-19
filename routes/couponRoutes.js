66e014e4bc2fbcb24b89ab007f07bffbb2d5d3a7
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-18 18:07:47 -0600

    fix: actualizar verificación de roles en la ruta getCoupons

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -5,7 +5,7 @@ const CouponController = require('../controllers/couponController');
 const express = require('express');
 const router = express.Router();
 
-router.get('/getCoupons', auth, checkRole([1, 3]), CouponController.getAllCoupons);
+router.get('/getCoupons', auth, checkRole([1,2, 3]), CouponController.getAllCoupons);
 router.get('/getRanking', auth, checkRole([1, 3]), CouponController.getRanking);
 router.post('/createCoupon', auth, checkRole([1, 3]), CouponController.createCoupon);
 
