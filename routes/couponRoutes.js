42ab0742922e4b4e513e4186173bc9f98d3d28ea
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-04-21 22:20:03 -0600

    fix: eliminar el parámetro cupon_id de la ruta de eliminación de cupones

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.get('/getCoupons', auth, checkRole([1,2, 3]), CouponController.getAllCoupons);
 router.get('/getRanking', auth, checkRole([1, 3]), CouponController.getRanking);
 router.post('/createCoupon', auth, checkRole([1, 3]), CouponController.createCoupon);
-router.put('/deleteCoupon/:cupon_id', auth, checkRole([1, 3]), CouponController.deleteCoupon);
+router.put('/deleteCoupon', auth, checkRole([1, 3]), CouponController.deleteCoupon);
 
 module.exports = router;
