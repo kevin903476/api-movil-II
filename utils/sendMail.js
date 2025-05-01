e0c34527086b3032049114621d636ab1e5a29e9a
AdonayXX <emdiazdu@est.utn.ac.cr>
2025-05-01 00:12:47 -0600

    fix: actualizar puerto SMTP a 465 y habilitar conexión segura

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -11,7 +11,7 @@ console.log('SMTP_PASS =', process.env.SMTP_PASS);
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
